cc5ce2346e654844ebc988b9cfea6c834946c070
mtcarrick <mtcarrick@alaska.edu>
2015-12-14 16:04:36 -0900

    Create morse.py

---
+++ morse.py
@@ -0,0 +1,46 @@
+import re
+
+def convToMorse(str):
+
+    morseDict = {'a':'.-', 'b':'-...', 'c':'-.-.',
+                 'd':'-..', 'e':'.', 'f':'..-.',
+                 'g':'--.', 'h':'....', 'i':'..',
+                 'j':'.---', 'k':'-.-', 'l':'.-..',
+                 'm':'--', 'n':'-.', 'o':'---',
+                 'p':'.--.', 'q':'--.-', 'r':'.-.',
+                 's':'...', 't':'-', 'u':'..-',
+                 'v':'...-', 'w':'.--', 'x':'-..-',
+                 'y':'-.--', 'z':'--..',
+
+                 '1':'.----', '2':'..---', '3':'...--',
+                 '4':'....-', '5':'.....', '6':'-....',
+                 '7':'--...', '8':'---..', '9':'----.',
+                 '0':'-----',
+
+                 '.':'.-.-.-', ',':'--..--', '?':'..--..'}
+
+    working = list(str)
+
+    result = []
+
+    for i in range(0,len(working)):
+        result.append(morseDict[working[i].lower()]) # remember to convert to lower case
+        
+    return result    
+
+def printMorse(list):
+
+    str = ""
+    for i in range(0,len(list)):
+       str = str + list[i] + " "
+
+    print(str)
+
+if __name__ == '__main__':
+    str = input('Enter your string: ')
+
+    try:
+        printMorse(convToMorse(str))
+    except:
+        print('Error! Can only take messages containing:\n\tLetters a-z\n\tDigits 1-9\n\tSpecial characters ? , .')
+        input()
